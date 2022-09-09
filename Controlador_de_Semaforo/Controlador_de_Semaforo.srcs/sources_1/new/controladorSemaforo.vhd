5504d616fcdfae84e0a848568a2e0dff39114c13
Guilherme Costa <guilhermercosta1@gmail.com>
2022-09-09 18:13:51 -0300

    TopLevel

--- Controlador_de_Semaforo/Controlador_de_Semaforo.srcs/sources_1/new/controladorSemaforo.vhd
+++ Controlador_de_Semaforo/Controlador_de_Semaforo.srcs/sources_1/new/controladorSemaforo.vhd
@@ -32,7 +32,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity controladorSemaforo is
-    Port ( Vs : in STD_LOGIC;
+    Port ( CLK: in STD_LOGIC;
+           Vs : in STD_LOGIC;
            TL : in STD_LOGIC;
            Ts : in STD_LOGIC;
            Mg : out STD_LOGIC;
@@ -44,7 +45,33 @@ entity controladorSemaforo is
 end controladorSemaforo;
 
 architecture Behavioral of controladorSemaforo is
-
+    --instanciando componentes
+    component LogicaSequencial
+        Port ( Vs : in STD_LOGIC;
+               CLKin : in STD_LOGIC;
+               TL : in STD_LOGIC;
+               Ts : in STD_LOGIC;
+               G : out STD_LOGIC_VECTOR (0 to 1));
+        end component;
+    component DecodificadorGray
+        Port ( G : in STD_LOGIC_VECTOR (0 to 1);
+               S : out STD_LOGIC_VECTOR (0 to 3));
+    end component;
+    component DivisorDeFrequencia
+        Port ( CLKin : in STD_LOGIC;
+               CLKout : out STD_LOGIC);
+    end component;
+    component Seletor
+        Port ( S1 : in STD_LOGIC;
+               S3 : in STD_LOGIC;
+               TL : out STD_LOGIC;
+               Ts : out STD_LOGIC);
+    end component;
+    component Timer
+        Port ( CLK : in STD_LOGIC;
+               TL : out STD_LOGIC;
+               Ts : out STD_LOGIC);
+    end component;
 begin
 
 

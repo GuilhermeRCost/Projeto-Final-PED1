d99099923a2b73c4d6425343288eea7445801b72
Guilherme Costa <guilhermercosta1@gmail.com>
2022-09-21 14:14:50 -0300

    Docodificação dos estados

--- Controlador_de_Semaforo/Controlador_de_Semaforo.srcs/sources_1/new/Maquina_de_Estados.vhd
+++ Controlador_de_Semaforo/Controlador_de_Semaforo.srcs/sources_1/new/Maquina_de_Estados.vhd
@@ -34,8 +34,6 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity Maquina_de_Estados is
     Port ( CLK: in STD_LOGIC;
            Vs : in STD_LOGIC;
-           TL : in STD_LOGIC;
-           Ts : in STD_LOGIC;
            Mg : out STD_LOGIC;
            My : out STD_LOGIC;
            Mr : out STD_LOGIC;
@@ -46,14 +44,49 @@ end Maquina_de_Estados;
 
 architecture Behavioral of Maquina_de_Estados is
     type state is (e1, e2,e3, e4);
+    signal Ts, Tl : std_logic ;
+    signal sM, sS: std_logic_vector (0 to 2) ;
     signal cState, nextState: state;
+    
+    --sinais para o clock
+   -- signal clk_5s, clk_25s : std_logic := '0' ;
+   -- signal prescaler5 : integer range 0 to 500_000_000 := 500_000_000; 
+   -- signal counter5 : integer range 1 to 500_000_000 := 1;
 begin
-
+    Mg <= sM(0);
+    My <= sM(1);
+    Mr <= sM(2);
+    
+    Sg <= sS(0);
+    Sy <= sS(1);
+    Sr <= sS(2);
+    
+    DecodificaEstado: process(cstate, sM, sS)
+    begin
+        case cState is
+          when  e1 =>
+               sM <= "100"; --verde
+               sS <= "001";  --vermelho
+               
+          when  e2 =>
+               sM <= "010"; --amarelo
+               sS <= "001"; --vermelho
+          when  e3 =>
+               SM <= "001"; --vermelho
+               sS <= "100"; --verde
+          when  e4 =>
+               sM <= "001"; --vermelho
+               sS <= "010"; --amarelo
+       end case;
+    end process;
+    
+    
+    
     armazena_estado: process(clk)
     begin
-        if (rising_edge(clk)) then
-            cState <= nextState;
-        end if;
+       if (rising_edge(clk)) then
+           cState <= nextState;
+       end if;
     end process;
     
      transicao_estado: process(cstate, vs, tl, ts)
